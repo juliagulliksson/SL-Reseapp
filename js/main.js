e8c914baf0d1d257a7de2069321501cf26786e91
juliagulliksson <julia.gulliksson@medieinstitutet.se>
2018-03-09 10:54:21 +0100

    working on destination options function

--- js/main.js
+++ js/main.js
@@ -1,19 +1,77 @@
-const div = document.getElementById('output');
+const outputDiv = document.getElementById('output');
 const departureKey = "9dcc27c7806146308e9a817303722483";//Reseplanerare 3
 const placeKey = "c3b5d6f2b9a1421185d7fa4e7e951daf";//Platsuppslag
+const originInput = document.getElementById('origin');
+const dataList = document.getElementById('destinationOrigin');
+const submitButton = document.getElementById('submitSearch');
+const form = document.getElementById('searchForm');
+
+form.addEventListener('submit', function(event){
+    event.preventDefault(); 
+});
+
+submitButton.addEventListener('click', function(){
+    const inputValue = originInput.value;
+    showDestinations(inputValue);
+})
+
+/*originInput.addEventListener('keyup', function(){
+    const inputValue = originInput.value;
+    showDestinations(inputValue);
+});*/
 
 function fetchDepartures(){
-    fetch('https://cors-anywhere.herokuapp.com/http://api.sl.se/api2/TravelplannerV3/trip.json?key=' + departureKey + '&originId=9626&destId=1002&searchForArrival=0&lang=sv')
-        .then(function(response) {
-        return response.json();
-      })
-      .then(function(departureData){
-          //console.log(departureData);
+    fetch('https://cors-anywhere.herokuapp.com/http://api.sl.se/api2/TravelplannerV3/trip.json?key=' + departureKey + '&originId=9625&destId=1002&searchForArrival=0&lang=sv')
+      .then((response) => response.json())
+      .then((departureData) => {
           displayDepartures(departureData);
       })
-      .catch(function(error){
+      .catch((error) => {
           console.log(error);
       });
+}
+
+function showDestinations(inputValue){
+    fetch('https://cors-anywhere.herokuapp.com/http://api.sl.se/api2/typeahead.json?key=' + placeKey + '&searchstring=' + inputValue)
+      .then((response) => response.json())
+      .then((destinationData) => {
+          console.log(destinationData);
+          displayDestinationOptions(destinationData);
+      })
+      .catch((error) => {
+          console.log(error);
+      });
+}
+
+/*function displayDestinationOptions(destinationData){
+    let destinationOption = ``;
+    console.log(destinationData.ResponseData);
+
+    for(i in destinationData.ResponseData){
+        destinationOption += `<option value="${destinationData.ResponseData[i].Name}" id="${destinationData.ResponseData[i].SiteId}"></option>`;
+    }
+
+    dataList.innerHTML += destinationOption;
+    if (dataList.innerHTML == ""){
+        alert("Hej");
+    }
+}*/
+
+function displayDestinationOptions(destinationData){
+    const outputDiv = document.getElementById('searchOutput');
+    let destinationOption = `<ul>`;
+
+    for(i in destinationData.ResponseData){
+        destinationOption += `<li>${destinationData.ResponseData[i].Name}
+        <input type="hidden" value="${destinationData.ResponseData[i].SiteId}"></li>`;
+    }
+    destinationOption += `</ul>`;
+    outputDiv.innerHTML = destinationOption;
+
+
+
+
+
 }
 
 fetchDepartures();
@@ -25,36 +83,45 @@ function displayDepartures(departureData){
       <p>  Från: ${departureData.Trip[0].LegList.Leg[0].Origin.name} </p>
       <p>  Till: ${departureData.Trip[0].LegList.Leg.slice(-1)[0].Destination.name} </p>
     `;
-    div.innerHTML += destinations;
+    outputDiv.innerHTML += destinations;
 
     let departureInfo = ``;
     
+    //Loop to display the departures
     for(i in departureData.Trip){
         departureInfo += `<div class="trip-wrapper">`;
-        console.log(departureData.Trip[i]);
+        
         for(j in departureData.Trip[i].LegList.Leg){
-            departureInfo += `
-            <div class="destination-wrapper">
-                <p>  Avgång: ${departureData.Trip[i].LegList.Leg[j].Origin.time} </p>
-                <p>  Ankomst: ${departureData.Trip[i].LegList.Leg[j].Destination.time} </p>
-            `;
 
-            if (departureData.Trip[i].LegList.Leg[j].direction != undefined) {
-                departureInfo += `<p>  Ta ${departureData.Trip[i].LegList.Leg[j].Product.name} 
-                mot ${departureData.Trip[i].LegList.Leg[j].direction}</p>`;
-            } else if (departureData.Trip[i].LegList.Leg[j].type == "WALK") {
-                departureInfo += `<p>  Gå till ${departureData.Trip[i].LegList.Leg[j].Destination.name}</p>`;
+            const departure = departureData.Trip[i].LegList.Leg[j];
+
+            departureInfo += `<div class="destination-wrapper">`;
+
+            if (departure.direction != undefined) {
+                departureInfo += `<p>  Ta ${departure.Product.name} 
+                                 mot ${departure.direction}</p>`;
+            } else if (departure.type == "WALK") {
+                departureInfo += `<p>  Gå till ${departure.Destination.name}</p>`;
             } else {
-                departureInfo += `<p>  Mot: ${departureData.Trip[i].LegList.Leg[j].Destination.name}</p>`;
+                departureInfo += `<p> Ta ${departure.Product.name} 
+                                     mot: ${departure.Destination.name}</p>`;
             }
-           
+
+            if (departure.type != "WALK") {
+                departureInfo += `
+                    <p>  Avgång: ${departure.Origin.time} </p>
+                    <p>  Ankomst: ${departure.Destination.time} </p>
+                `;
+            }
+
            departureInfo += `</div>`;
            
         }
         
         departureInfo += `</div>`;
     }
-    div.innerHTML = departureInfo;
+
+    outputDiv.innerHTML += departureInfo;
    
 }
-//<p>  Ta: ${departureData.Trip[i].LegList.Leg[j].Product.name}</p>
+
